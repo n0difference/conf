2ce7ae8eb9e88dbf7acd3e3e896a06f075c155e5
n0difference <134058450+n0difference@users.noreply.github.com>
2026-08-23 01:08:33 +0000

    Update and rename init.vim to init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,17 @@
+vim.cmd.syntax('on')
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+
+vim.cmd.colorscheme("lunaperche")
--- nvim/init.vim
+++
@@ -1,7 +0,0 @@
-syntax on
-set number relativenumber
-set cursorline
-set tabstop =4
-set shiftwidth =4
-set expandtab
-
